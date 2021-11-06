8021bc7a591703304addaf50b55f9fb97845ea39
Vlad Suhomlinov <suhomlinov.dev@gmail.com>
2021-11-06 10:30:40 +0300

    feat: корректировки записи

--- Processing/Generator.py
+++ Processing/Generator.py
@@ -8,16 +8,16 @@ from Helpers.Const import *
 class Generator:
 
     @staticmethod
-    def parse_and_generate(symbols: [str], duration=.25, volume=.25, rate=44100) -> [Signal]:
+    def generate_from(symbols: str, duration=.25, volume=.25, rate=44100) -> [Signal]:
         generated = []
 
         for symbol in symbols:
-            generated.append(Generator.generate(symbol, duration, volume, rate))
+            generated.append(Generator.calculate(symbol, duration, volume, rate))
 
         return generated
 
     @staticmethod
-    def generate(symbol: str, duration=.25, volume=.25, rate=44100) -> Signal:
+    def calculate(symbol: str, duration=.25, volume=.25, rate=44100) -> Signal:
         first_garmonik = GarmonikModel(volume, DTMF_TABLE[symbol.upper()][0], 0, int(duration * rate), 1, 1 / rate)\
             .trend(0, None)
         second_garmonik = GarmonikModel(volume, DTMF_TABLE[symbol.upper()][1], 0, int(duration * rate), 1, 1 / rate)\
--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 from Processing.Generator import *
 from IO.Write import *
+from Drivers.SpecInfoDisplayDriver import *
+
+import tkinter as tk
 
 
 if __name__ == '__main__':
+    # Устанавливаем окно и обьект графиков
+    window = tk.Tk()
+    window.title("Методы обработки экспериментальных данных")
+    display_model = SpecInfoDisplayDriver(window)
+
+    # ---------------
+    # Подсчеты
     write_file_path = "/Users/vladsuhomlinov/DTMF/sound.wav"
 
-    numbers = '123456789ABCD'
-    symbols = Generator.parse_and_generate(numbers)
+    numbers = '1'
+    symbols = Generator.generate_from(numbers, 0.1)
 
     Writer.write(write_file_path, symbols)
+
+    # ---------------
+    # Графики
+
+    from scipy.io.wavfile import read
+
+    data_y = read(write_file_path)[1][0:1024]
+    data_x = []
+
+    for i in range(len(data_y)):
+        data_x.append(i)
+
+    display_model.plot([data_x, data_y])
+
+    # Запускаем
+    display_model.display()
+    window.mainloop()
 
     print('Успех!')
